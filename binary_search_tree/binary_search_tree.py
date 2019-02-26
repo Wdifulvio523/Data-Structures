8b305505e1cf79a563bfba8b42d08f8118e53db5
William DiFulvio <w.difulvio523@gmail.com>
2019-02-26 18:35:11 -0500

    completed all functions for binary_search_tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,65 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    # store variables for self, and a boolean
+    current = self
+    complete = False
+    # Begin loop using the boolean == false
+    while not complete:
+    # compare values
+        # if current val > val
+        if current.value > value:
+            # check if there is a left
+            if current.left:
+                # if there is a left, set the left to the new current
+                current = current.left
+            else:
+                # if no left, set the new left to the value (create new BST node)
+                current.left = BinarySearchTree(value)
+                complete = True
+        # if current val < val  
+        else:
+            # check if there is a right, same logic as above
+            if current.right:
+                current = current.right
+            else:
+                current.right = BinarySearchTree(value)
+                complete = True
+
 
   def contains(self, target):
-    pass
+    
+    current = self
+    complete = False
+    
+    while not complete:
+      
+      # if there is nothing left to check, return false
+      if not current:
+        return False
+      
+      # if the value of the current node is == target, 
+      if current.value == target:
+        return True
+      
+      # if the current value is greater than the target 
+      elif current.value > target:    
+        current = current.left
+      
+      # if current value is less than the target 
+      else:
+        current = current.right
+
 
   def get_max(self):
-    pass
+    current = self
+    max = 0 
+    
+    while current:
+            
+      # if the current value is greater than the max  
+      if current.value > max: 
+        max = current.value   
+        current = current.right
+
+    return max
